853a8bc7953940a8bcbe37258fb2ba4bb2f505cb
Danilo Bargen <mail@dbrgn.ch>
2020-11-17 00:56:57 +0100

    Reorganize entry point, create SpaceApiParser file
    By ensuring that user code goes through these functions, we can add
    additional validation code (e.g. verifying that the input is not empty,
    and that it doesn't use an old and unsupported API version).

---
+++ src/test/kotlin/io/spaceapi/ParserTestJava.java
@@ -0,0 +1,25 @@
+package io.spaceapi;
+
+import io.spaceapi.types.Status;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParserTestJava {
+    private static String minimalV14Data = "{\n" +
+        "\"api_compatibility\": [\"14\"],\n" +
+        "\"space\": \"Coredump\",\n" +
+        "\"logo\": \"https://www.coredump.ch/wp-content/uploads/2016/11/logo.png\",\n" +
+        "\"url\": \"https://www.coredump.ch/\",\n" +
+        "\"location\": {\"lon\": 47.2251, \"lat\": 8.8339},\n" +
+        "\"contact\": {}\n" +
+    "}";
+
+    /**
+     * This test is here to test the Java API generated from Kotlin code.
+     */
+    @Test
+    public void testApi() {
+        final Status statusFromString = SpaceApiParser.parseString(minimalV14Data);
+        Assert.assertEquals("Coredump", statusFromString.getSpace());
+    }
+}
