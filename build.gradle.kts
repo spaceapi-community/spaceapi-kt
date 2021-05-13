0d46b11d34bd40cfa5276a213b5276717f14ff01
Danilo Bargen <mail@dbrgn.ch>
2021-05-13 23:42:51 +0200

    Update kotlinx-serialization-json to 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // Use serialization library
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
     // Kotlin helper libraries
     testImplementation("org.jetbrains.kotlin:kotlin-test")
